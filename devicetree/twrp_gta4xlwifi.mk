bf53d730d88a44c3a907060d67a0e4c970e2b6fb
bq <hqosts200@gmail.com>
2026-01-05 18:39:20 +0100

    Corrected PRODUCT_NAME

--- devicetree/twrp_gta4xlwifi.mk
+++ devicetree/twrp_gta4xlwifi.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_NAME := omni_gta4xlwifi
+PRODUCT_NAME := twrp_gta4xlwifi
 PRODUCT_DEVICE := gta4xlwifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-P610
